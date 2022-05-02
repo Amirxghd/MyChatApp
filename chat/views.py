984ad96546be328ea409570f26082de783cbd15e
amirreza <aghafari@kilid.com>
2022-05-03 01:13:47 +0430

    change to session mode

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
-from PrivateChat.models import PrivateChatRoom
+from PrivateChat.models import PrivateChatRoom, PrivateRoomChatMessage
 from public_chat.models import PublicChatRoom
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
@@ -10,25 +10,34 @@ import json
 
 DEBUG = False
 
-
-def chat(request, *args, **kwargs):
+@login_required(login_url='login')
+def chat_view(request, *args, **kwargs):
 
     context = {}
     user = request.user
 
-    # 1. Find all the rooms this user is a part of
+    account_id = request.session.get('account_id')
+
+    if account_id:
+        try:
+            other_user = Account.objects.get(id=account_id)
+        except Account.DoesNotExist:
+            return HttpResponse('account does not exists')
+        if other_user != request.user:
+            room = find_or_create_private_chat(request.user, other_user)
+            context['main_user'] = {
+                'friend': other_user,
+                'room': room
+            }
+
+    # 1. Find all the users private chats
     private_rooms1 = PrivateChatRoom.objects.filter(user1=request.user)
     private_rooms2 = PrivateChatRoom.objects.filter(user2=request.user)
     group_room = user.registered_users.all()
 
     # 2. merge the lists
-    private_rooms = list(chain(private_rooms1, private_rooms2))
+    private_rooms = private_rooms1.union(private_rooms2)
 
-    """
-    m_and_f:
-        [{"message": "hey", "friend": "Mitch"}, {"message": "You there?", "friend": "Blake"},]
-    Where message = The most recent message
-    """
     m_and_f = []
     for room in private_rooms:
         # Figure out which user is the "other user" (aka friend)
@@ -37,11 +46,10 @@ def chat(request, *args, **kwargs):
         else:
             friend = room.user1
         m_and_f.append({
-            'message': "", # blank msg for now (since we have no messages)
-            'friend': friend
+            'friend': friend,
+            'room': room,
         })
     context['m_and_f'] = m_and_f
-    # context['m_and_f'] = []
 
     groups = []
     for room in group_room:
@@ -58,32 +66,14 @@ def chat(request, *args, **kwargs):
     return render(request, "chat/chat.html", context)
 
 
-# Ajax call to return a private chatroom or create one if does not exist
-def create_or_return_private_chat(request, *args, **kwargs):
-    user1 = request.user
-    payload = {}
-    if user1.is_authenticated:
-        if request.method == "POST":
-            user2_id = request.POST.get("user2_id")
-            try:
-                user2 = Account.objects.get(pk=user2_id)
-                chat = find_or_create_private_chat(user1, user2)
-                payload['response'] = "Successfully got the chat."
-                payload['chatroom_id'] = chat.id
-            except Account.DoesNotExist:
-                payload['response'] = "Unable to start a chat with that user."
-    else:
-        payload['response'] = "You can't start a chat if you are not authenticated."
-    return HttpResponse(json.dumps(payload), content_type="application/json")
-
-
 def find_or_create_private_chat(user1, user2):
     try:
         chat = PrivateChatRoom.objects.get(user1=user1, user2=user2)
+        return chat
     except PrivateChatRoom.DoesNotExist:
         try:
             chat = PrivateChatRoom.objects.get(user1=user2, user2=user1)
         except PrivateChatRoom.DoesNotExist:
             chat = PrivateChatRoom(user1=user1, user2=user2)
             chat.save()
-    return chat
+        return chat
