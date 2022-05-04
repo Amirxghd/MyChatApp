e6b2d41014df51d902146aa6616f66dcac889385
amirreza <aghafari@kilid.com>
2022-05-04 14:24:08 +0430

    change urls

--- public_chat/urls.py
+++ public_chat/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from public_chat import views
 
 urlpatterns = [
-    path('group/<str:username>/', views.group_chat, name='group_chat'),
     path('create/', views.create_group, name='create_group'),
     path('join/<str:invite_link>/', views.join_to_group, name='join_group'),
     path('edit/<str:username>/', views.edit_group, name='edit_room'),
-    path('remove/<str:username>/', views.remove_group, name='remove_group'),
-    path('exit/<str:username>/', views.exit_group, name='exit_group'),
+    path('remove_or_exit/<str:group_id>/', views.remove_or_exit_group, name='remove_or_exit_group'),
     path('reset_invite_link/<str:group_id>/', views.reset_invite_link, name='reset_invite_link'),
+    path('show_invite_link/<str:group_id>/', views.show_invite_link, name='show_invite_link'),
+    path('show_memebers/<str:group_id>/', views.show_members, name='show_members'),
     ]
