af5325b3581e3f1bb4c2a9d344d2917107b620ef
amirreza <aghafari@kilid.com>
2022-05-04 22:52:41 +0430

    change username to group_id

--- public_chat/urls.py
+++ public_chat/urls.py
@@ -4,7 +4,7 @@ from public_chat import views
 urlpatterns = [
     path('create/', views.create_group, name='create_group'),
     path('join/<str:invite_link>/', views.join_to_group, name='join_group'),
-    path('edit/<str:username>/', views.edit_group, name='edit_room'),
+    path('edit/<str:group_id>/', views.edit_group, name='edit_room'),
     path('remove_or_exit/<str:group_id>/', views.remove_or_exit_group, name='remove_or_exit_group'),
     path('reset_invite_link/<str:group_id>/', views.reset_invite_link, name='reset_invite_link'),
     path('show_invite_link/<str:group_id>/', views.show_invite_link, name='show_invite_link'),
